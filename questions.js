bf7e970752428a8d6548341180b1cfdb2b72202e
Tabrez Alam <48343287+tabreza3500@users.noreply.github.com>
2021-03-26 01:22:10 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,59 @@
+
+let questions = [
+    {
+    numb: 1,
+    question: "BEST UNIVERSITY IN GREATER NOIDA?",
+    answer: "SHARDA UNIVERSITY",
+    options: [
+      "SHARDA UNIVERSITY",
+      "GAUTAM BUDH UNIVERSITY",
+      "GALGOTIA UNIVERSITY",
+      "NOIDA INTERNATIONAL UNIVERSITY"
+    ]
+  },
+    {
+    numb: 2,
+    question: "HOW MANY BUILDING IN SHARDA UNIVERSITY?",
+    answer: "THIRTY",
+    options: [
+      "TWENTY",
+      "TEN",
+      "THIRTY",
+      "FIFTY"
+    ]
+  },
+    {
+    numb: 3,
+    question: "HOW MANY TEACHERS IN SHARDA SET?",
+    answer: "TWENTY",
+    options: [
+      "TWENTY",
+      "FIFTEEN",
+      "NINE",
+      "TWENTY FIVE"
+    ]
+  },
+    {
+    numb: 4,
+    question: "HOW MANY STUDENTS IN SET CSE?",
+    answer: "THREE HUNDREDS",
+    options: [
+      "FIVE HUNDREDS",
+      "THREE HUNDREDS",
+      "FOUR HUNDREDS",
+      "NONE OF THE ABOVE"
+    ]
+  },
+    {
+    numb: 5,
+    question: "HOW MANY ROOMS IN BLOCK ONE ?",
+    answer: "TWENTY FIVE",
+    options: [
+      "FIFTY FIVE",
+      "FOURTY FIVE",
+      "THIRTY FIVE",
+      "TWENTY FIVE"
+    ]
+  },
+ 
+];
